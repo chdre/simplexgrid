dadeddb5ee48e657e6b6c83c77b4c8cf89a45b38
chrdre <christerdr@outlook.com>
2022-01-18 14:40:18 +0100

    bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as infile:
 
 setuptools.setup(
     name='simplexgrid',
-    version='0.3.2',
+    version='0.3.3',
     author='Christer Dreierstad',
     author_email='christerdr@outlook.com',
     description='Create grid-like Simplex noise',
